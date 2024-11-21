35f4cbdc4fe6832579518e569a1f315f87710029
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-11-21 10:06:23 +0300

    configure kafika consumer and producer

---
+++ src/main/java/com/kafkaspringboot/spring/boot/kafka/project/SpringBootKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaspringboot.spring.boot.kafka.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaspringboot/spring/boot/kafka/project/SpringBootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaspringboot.spring.boot.kafka.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
